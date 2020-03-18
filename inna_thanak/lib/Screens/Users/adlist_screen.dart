e69259e538513ca72e1207d392841a23487f7f25
Deepthashash <shashideepshashi@outlook.com>
2020-03-18 21:49:35 +0530

    Implemented addList

--- inna_thanak/lib/Screens/Users/adlist_screen.dart
+++ inna_thanak/lib/Screens/Users/adlist_screen.dart
@@ -27,11 +27,66 @@ class _AdListState extends State<AdList> {
   }
 
   Widget _body(){
-    return Center(
-        child: InkWell(
-          child: Text("bodim places list (UI is in the group)"),
-          onTap: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>SingleAd())),
-        )
-      );
+    return ListView(
+      children: <Widget>[
+        Container(
+            padding: EdgeInsets.only(top: 10.0, left: 5.0, right: 5.0),
+            child: InkWell(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: new FractionalOffset(0.0, 1.0),
+                    child: Text("Borella",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),)
+                  ),
+                  SizedBox(height: 10.0,),
+                  Container(
+                    // height: 230,
+                    child: Image.network("http://www.bayfieldinn.com/sites/bayfieldinn.com/files/content/rentals/DSC_0097.JPG")
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: Text("rooms:1 beds:3 baths:2")),
+                        Expanded(child: Text("Rs.2000/month",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              onTap: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>SingleAd())),
+              
+            )
+          ),
+          Container(
+            padding: EdgeInsets.only(top: 10.0, left: 5.0, right: 5.0),
+            child: InkWell(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: new FractionalOffset(0.0, 1.0),
+                    // padding: EdgeInsets.only(right: 315.0),
+                    child: Text("Nugegoda",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),)
+                  ),
+                  SizedBox(height: 10.0,),
+                  Container(
+                    // height: 230,
+                    child: Image.network("http://www.bayfieldinn.com/sites/bayfieldinn.com/files/content/rentals/DSC_0084.JPG")
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(child: Text("rooms:1 beds:3 baths:2")),
+                        Expanded(child: Text("Rs.2000/month",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0)))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              onTap: ()=>Navigator.push(context,MaterialPageRoute(builder: (context)=>SingleAd())),
+              
+            )
+          ),
+      ],
+    );
   }
 }
