5b77191920327f4c907984e280f72b7ddf645c81
sandunsameera <sandunsameera25@gmail.com>
2020-04-06 21:48:35 +0530

    Implement lable text form field widget

--- inna_thanak/lib/Screens/postad_screen.dart
+++ inna_thanak/lib/Screens/postad_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:inna_thanak/Screens/Resources/resources.dart';
+import 'package:inna_thanak/Widgets/lable_text_field.dart';
 
 class PostAdScreen extends StatefulWidget {
   @override
@@ -6,16 +8,77 @@ class PostAdScreen extends StatefulWidget {
 }
 
 class _PostAdScreenState extends State<PostAdScreen> {
+  //final GlobalKey _formKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _body(),
-    );
-  }
-  Widget _body(){
-    return Container(
-      child: Center(
-        child: Text("Posting ads"),
+      resizeToAvoidBottomPadding: false,
+      body: SingleChildScrollView(
+        child: Form(
+            child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.fromLTRB(120, 5, 120, 10),
+              child: Container(
+                  child: ButtonTheme(
+                      child: IconButton(
+                icon: Icon(Icons.add_a_photo,color: Color(Resources.primarycolor),),
+                onPressed: () {
+                  print("Tapped");
+                },
+                iconSize: 100,
+              ))),
+            ),
+            Container(
+              width: MediaQuery.of(context).size.width - 20,
+              child: LabelTextField(
+                hintText: "Price",
+                labelText: "Price",
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width - 20,
+              child: LabelTextField(
+                hintText: "Key money",
+                labelText: "Key money expected",
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width - 20,
+              child: LabelTextField(
+                hintText: "Address",
+                labelText: "Location address",
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width - 20,
+              child: LabelTextField(
+                hintText: "Rooms",
+                labelText: "Number of rooms",
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width - 20,
+              child: LabelTextField(
+                hintText: "Beds",
+                labelText: "Number of beds",
+              ),
+            ),
+            SizedBox(height: 10),
+            Container(
+              width: MediaQuery.of(context).size.width - 20,
+              child: LabelTextField(
+                hintText: "Description",
+                labelText: "Description",
+              ),
+            ),
+          ],
+        )),
       ),
     );
   }
